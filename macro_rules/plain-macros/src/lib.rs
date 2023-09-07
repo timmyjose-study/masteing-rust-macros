99a6c79e2c4115ce58124d9114a06c68bbf535bf
Timmy Jose <zoltan.jose@gmail.com>
2023-09-07 11:21:27 +0530

    Initial commit - simple my_vec macro.

---
+++ macro_rules/plain-macros/src/lib.rs
@@ -0,0 +1,26 @@
+macro_rules! my_vec {
+    ( $( $x:expr ),* ) => {
+        {
+        let mut v = Vec::new();
+
+        $(
+            v.push($x);
+        )*
+        v
+
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_my_vec() {
+        let empty_left: Vec<i32> = my_vec![];
+        let empty_right: Vec<i32> = vec![];
+
+        assert_eq!(empty_left, empty_right);
+        assert_eq!(my_vec![1], vec![1]);
+        assert_eq!(my_vec![1, 2, 3, 4, 5], vec![1, 2, 3, 4, 5]);
+    }
+}
